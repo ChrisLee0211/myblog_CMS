fad4d521eeaee3358b9b0b84bd79b492347c9b0a
李铭辉 <1018596741@qq.com>
2020-06-17 23:19:04 +0800

    配置apollo

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from 'react-redux';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import { Provider } from 'react-redux';
 import store from './store'
 import * as serviceWorker from './serviceWorker';
+const client = new ApolloClient({
+    uri: "http://localhost:3000/"
+});
 
 ReactDOM.render(
-    <Provider store={store}>
-            <App/> 
-    </Provider>,
-document.getElementById('root'));
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
